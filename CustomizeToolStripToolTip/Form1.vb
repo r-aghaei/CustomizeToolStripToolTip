9769ff33183547e75f3f50b31c7729f3f783450e
r-aghaei <r.aghaei@outlook.com>
2023-02-02 17:53:10 +0100

    Added the example and readme.

--- CustomizeToolStripToolTip/Form1.vb
+++ CustomizeToolStripToolTip/Form1.vb
@@ -1,3 +1,33 @@
 ﻿Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        SetupCustomDraw(MenuStrip1)
+        SetupCustomDraw(ToolStrip1)
+    End Sub
+    Sub SetupCustomDraw(MyToolStrip As ToolStrip)
+        Dim MyField = MyToolStrip.GetType().GetProperty("ToolTip",
+            Reflection.BindingFlags.NonPublic Or Reflection.BindingFlags.Instance)
+        Dim MyToolTip = CType(MyField.GetValue(MyToolStrip), ToolTip)
+        MyToolTip.OwnerDraw = True
+        MyToolTip.BackColor = Color.Red
+        MyToolTip.ForeColor = Color.White
+        AddHandler MyToolTip.Popup,
+            Sub(obj, args)
+                args.ToolTipSize = New Size(args.ToolTipSize.Width * 2, args.ToolTipSize.Height * 2)
+            End Sub
+        AddHandler MyToolTip.Draw,
+            Sub(obj, args)
+                Dim F = New Font(MyToolStrip.Font.FontFamily, MyToolStrip.Font.Size * 2)
+                args.DrawBackground()
+                TextRenderer.DrawText(args.Graphics, args.ToolTipText,
+                    F, args.Bounds, MyToolTip.ForeColor)
+                args.DrawBorder()
+            End Sub
 
+        'Passing each sub menu to the function
+        For Each mnuItem As ToolStripMenuItem In MyToolStrip.Items.OfType(Of ToolStripMenuItem)
+            If mnuItem.DropDown IsNot Nothing Then
+                SetupCustomDraw(mnuItem.DropDown)
+            End If
+        Next
+    End Sub
 End Class
--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # CustomizeToolStripToolTip
+
+This example shows hows to customize the ToolTip of ToolStrip, MenuStrip, ContextMenuStrip, or anything deriving from ToolStrip.
+
+ToolStrip has an internal ToolTip property that you can find using reflection, and then treat it like any normal tooltip, for example you can set its forecolor, backcolor, or change the font and size of it and have a custom logic for drawing the tooltip.
+
+The usage of the method is easy, just pass the MenuStrip or ToolStrip instance to it:
+
+    SetupCustomDraw(MenuStrip1)
+
+It will apply the customization on tooltip of all menu items and sub menus of the givem menu. You should also make sure the ShowToolTip property of the ToolStrip or MenuStrip is True.
+
+There you go!
+
+Look at the following customization; it's a tooltip with White text, Red backcolor, and twice size/font as normal:
+
+![Custom ToolStrip Tooltip](tooltip1.gif)
